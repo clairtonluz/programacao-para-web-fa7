71cf3e9b39c693b6e64da126dac2463c1f22b6a8
clairtonluz <clairton.c.l@gmail.com>
2016-02-12 12:14:58 -0300

    implementando login com filter

---
+++ src/main/java/br/edu/fa7/cursojsf/LoginBean.java
@@ -0,0 +1,44 @@
+package br.edu.fa7.cursojsf;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Named
+@RequestScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+
+    public void login() throws IOException {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        HttpSession session = (HttpSession) context.getSession(true);
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        session.setAttribute("userLogged", username);
+
+        context.redirect(request.getContextPath());
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/br/edu/fa7/cursojsf/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package br.edu.fa7.cursojsf.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("*.xhtml")
+public class AuthFilter implements Filter {
+    private ServletContext context;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.context = filterConfig.getServletContext();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String uri = req.getRequestURI();
+        this.context.log("Recurso::" + uri);
+
+        HttpSession session = req.getSession(false);
+
+        if (!uri.endsWith("login.xhtml") && (session == null || (session.getAttribute("userLogged") == null))) {
+            this.context.log("acesso nao autorizado");
+            res.sendRedirect("login.xhtml");
+        } else {
+            // pass the request along the filter chain
+            chain.doFilter(request, response);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
